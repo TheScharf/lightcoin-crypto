b1d194d6ffec44395015f38d5fb92676e9c89887
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-18 21:35:37 -0700

    money app, will add extra features when I have time (stretch)

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
-
 }
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return - this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+}
+
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
 
 console.log('Balance:', balance);
